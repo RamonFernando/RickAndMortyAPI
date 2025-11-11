a33d0621c8476c6450e7ecbeef5802e2c6477632
RamonFernando <ramonrfpa88@gmail.com>
2025-11-11 20:17:57 +0100

    agregando RickAndMortyAPI al rep

---
+++ Controllers/CharacterControler.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using RickAndMortyAPI.Models;
+using System.Net.Http;
+
+namespace RickAndMortyAPI.Controllers
+{
+    public class CharacterControler : Controller
+    {
+        static async void GetCharacters(int page)
+        { 
+            HttpClient client = new HttpClient();
+            string utl = $"https://rickandmortyapi.com/api/character?page={page}";
+            HttpResponseMessage response = await client.GetAsync(utl);
+
+            response.EnsureSuccessStatusCode();
+            Console.WriteLine((response.StatusCode == System.Net.HttpStatusCode.OK ? "OK" : "Error " + response.StatusCode));
+
+            string content = await response.Content.ReadAsStringAsync();
+
+            GetListCharacters(content);
+            
+        }
+        private static List<Character> GetListCharacters(string content)
+        {
+            List<Character> characters = new List<Character>();
+            return characters;
+        }
+
+
+    }
+}
---
+++ Models/Character.cs
@@ -0,0 +1,13 @@
+﻿namespace RickAndMortyAPI.Models
+{
+    public class Character
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Status { get; set; }
+        public string Species { get; set; }
+        public string Url { get; set; }
+        public string Gender { get; set; }
+        public string Image { get; set; }
+    }
+}
